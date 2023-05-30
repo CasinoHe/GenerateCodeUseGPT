03602aaafdacae54f90369302b7690012e20b5f5
casino <casinohe@outlook.com>
2023-05-30 14:45:51 +0800

    fix bug in judging whether the token is empty in setting panel

--- system/settings/settings.py
+++ system/settings/settings.py
@@ -104,7 +104,7 @@ class Settings(object):
         if self.slack_token and self.claude_user_id and self.general_channel_id:
             return False
 
-        return False
+        return True
 
     def InterfaceIsEmpty(self):
         '''
